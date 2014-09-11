1be58e81b1a37bc9375bb3d3ea2eb589ad9512e0
fizzmo <Joshua.a.mccoy@gmail.com>
2014-09-11 12:45:42 -0400

    Update data_gathering.R

--- data_gathering.R
+++ data_gathering.R
@@ -3,3 +3,7 @@ download.file("http://wveis.k12.wv.us/nclb/Library/download.cfm?filename=assessm
 download.file("http://wveis.k12.wv.us/nclb/Library/download.cfm?filename=assessment.csv&loc=?subg=TO&sy=12&cn=999&size=9&stype=0&ttype=0&SO=1&grade=03&sub=R", destfile="wv_12_03.csv")
 download.file("http://wveis.k12.wv.us/nclb/Library/download.cfm?filename=assessment.csv&loc=?subg=TO&sy=11&cn=999&size=9&stype=0&ttype=0&SO=1&grade=03&sub=R", destfile="wv_11_03.csv")
 download.file("http://wveis.k12.wv.us/nclb/Library/download.cfm?filename=assessment.csv&loc=?subg=TO&sy=10&cn=999&size=9&stype=0&ttype=0&SO=1&grade=03&sub=R", destfile="wv_10_03.csv")
+
+
+#School Location Data
+http://wvgis.wvu.edu/data/dataset.php?ID=180
