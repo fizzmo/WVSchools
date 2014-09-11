2673d4a3fb0731dc1a403d7baaec2efb3e5d2cf9
fizzmo <Joshua.a.mccoy@gmail.com>
2014-09-11 12:24:40 -0400

    Create data_gathering.R

---
+++ data_gathering.R
@@ -0,0 +1,2 @@
+#Source:  http://wveis.k12.wv.us/nclb/pub/assessment/grade3reading.cfm?sy=13&grade=03&sub=R&size=9&SO=1&subg=TO&cn=999&stype=0&ttype=0
+download.file("http://wveis.k12.wv.us/nclb/Library/download.cfm?filename=assessment.csv&loc=?subg=TO&sy=13&cn=999&size=9&stype=0&ttype=0&SO=1&grade=03&sub=R", destfile="wv_13_03.csv")
