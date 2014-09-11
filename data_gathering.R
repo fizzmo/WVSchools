887312f7ff8869f8b804d9953765c0d018d4db65
fizzmo <Joshua.a.mccoy@gmail.com>
2014-09-11 12:27:44 -0400

    Update data_gathering.R

--- data_gathering.R
+++ data_gathering.R
@@ -1,2 +1,5 @@
 #Source:  http://wveis.k12.wv.us/nclb/pub/assessment/grade3reading.cfm?sy=13&grade=03&sub=R&size=9&SO=1&subg=TO&cn=999&stype=0&ttype=0
 download.file("http://wveis.k12.wv.us/nclb/Library/download.cfm?filename=assessment.csv&loc=?subg=TO&sy=13&cn=999&size=9&stype=0&ttype=0&SO=1&grade=03&sub=R", destfile="wv_13_03.csv")
+download.file("http://wveis.k12.wv.us/nclb/Library/download.cfm?filename=assessment.csv&loc=?subg=TO&sy=12&cn=999&size=9&stype=0&ttype=0&SO=1&grade=03&sub=R", destfile="wv_12_03.csv")
+download.file("http://wveis.k12.wv.us/nclb/Library/download.cfm?filename=assessment.csv&loc=?subg=TO&sy=11&cn=999&size=9&stype=0&ttype=0&SO=1&grade=03&sub=R", destfile="wv_11_03.csv")
+download.file("http://wveis.k12.wv.us/nclb/Library/download.cfm?filename=assessment.csv&loc=?subg=TO&sy=10&cn=999&size=9&stype=0&ttype=0&SO=1&grade=03&sub=R", destfile="wv_10_03.csv")
